16bd73c941ee1f5be5734057ed91107dbf5d67a5
MarcosRC <mortazwarian@hotmail.com>
2025-11-16 08:18:18 -0300

    Adds repository interfaces
    Defines interfaces for interacting with data access layers.
    These interfaces include methods for common CRUD operations.

---
+++ WebAPI_Empresas/CnpjRegistry/src/WebAPI_Empresas.Application/Interfaces/IAtividadeSecundariaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAPI_Empresas.Domain.Entities;
+
+namespace WebAPI_Empresas.Application.Interfaces;
+
+public interface IAtividadeSecundariaRepository
+{
+    Task<AtividadeSecundaria?> GetByIdAsync(Guid id);
+    Task<IEnumerable<AtividadeSecundaria>> GetAllAsync();
+    Task AddAsync(AtividadeSecundaria entity);
+    Task UpdateAsync(AtividadeSecundaria entity);
+    Task DeleteAsync(Guid id);
+}
